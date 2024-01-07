8b152d9452f2b101597302f847d3f8de5e323445
tommy <thefamousdoge@hotmail.com>
2024-01-07 00:42:03 -0800

    make the auto installer install discordia dependencies it does it correctly, you will not need to mess with anything

--- install_discordia.bat
+++ install_discordia.bat
@@ -1,1 +1,3 @@
 .\exes\lit.exe install SinisterRectus/discordia
+.\exes\lit.exe install GitSparTV/discordia-slash
+git clone https://github.com/Bilal2453/discordia-interactions.git ./deps/discordia-interactions
