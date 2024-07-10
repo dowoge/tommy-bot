8298c9b5733658bfacba0a06386b79dcfdcf8d64
tommy <thefamousdoge@hotmail.com>
2024-07-10 15:48:26 -0400

    fix dependencies

--- install_discordia.bat
+++ install_discordia.bat
@@ -1,3 +1,3 @@
 .\exes\lit.exe install SinisterRectus/discordia
-.\exes\lit.exe install GitSparTV/discordia-slash
-git clone https://github.com/Bilal2453/discordia-interactions.git ./deps/discordia-interactions
+git clone git@github.com:GitSparTV/discordia-slash.git ./deps/discordia-slash
+git clone git@github.com:Bilal2453/discordia-interactions.git ./deps/discordia-interactions
