1c2fb8ba895081784c11a50df27cd512e11be1ed
tommy <thefamousdoge@hotmail.com>
2024-08-09 17:26:21 -0400

    change quotes + add period at the end of the sentence

--- src/SlashCommands/Link.lua
+++ src/SlashCommands/Link.lua
@@ -1,6 +1,6 @@
 local SlashCommandTools = require('discordia-slash').util.tools()
 
-local LINK_EXPLANATION_MESSAGE = [[Use the `!link <roblox username>` command with <@759490176119472178> to start the linking process, make sure your DMs are open so it can message you. For more information, use `!help link`]]
+local LINK_EXPLANATION_MESSAGE = 'Use the `!link <roblox username>` command with <@759490176119472178> to start the linking process, make sure your DMs are open so it can message you. For more information, use `!help link`.'
 
 local LinkCommand = SlashCommandTools.slashCommand('link', 'Explains how to link your Roblox account to your discord account')
 
