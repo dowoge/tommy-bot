912751724a6158efb4cef1fa57bcc136a143e7c9
tommy <thefamousdoge@hotmail.com>
2026-01-29 19:07:46 -0500

    Rank points calculation command

--- src/Modules/StrafesNET.lua
+++ src/Modules/StrafesNET.lua
@@ -9,26 +9,85 @@ local RequestHeaders = {
     ["X-API-Key"] = APIKeys.StrafesNET
 }
 
-function L1Copy(t, b)
+local function L1Copy(t, b)
     b = b or {}
     for x, y in next, t do b[x] = y end
     return b
 end
 
-local STRAFESNET_API_URL = "https://api.strafes.net/api/v1/"
-local FIVEMAN_API_URL = 'https://api.fiveman1.net/v1/'
-local ROBLOX_API_URL = 'https://users.roproxy.com/v1/'
-local ROBLOX_BADGES_API = 'https://badges.roproxy.com/v1/'
-local ROBLOX_PRESENCE_URL = 'https://presence.roproxy.com/v1/'
-local ROBLOX_THUMBNAIL_URL = 'https://thumbnails.roproxy.com/v1/'
-local ROBLOX_INVENTORY_API = 'https://inventory.roproxy.com/v1/'
-local ROBLOX_GROUPS_ROLES_URL = 'https://groups.roproxy.com/v2/users/%s/groups/roles'
+local function Round(Number, Digit)
+    return tonumber(string.format('%.' .. (Digit or 0) .. 'f', Number))
+end
 
-local GAME_IDS = {
-    BHOP = 1,
-    SURF = 2,
-    -- FLY_TRIALS = 5
-}
+local function SafeNumberToString(Value)
+    if Value == nil then return nil end
+    local ValueType = type(Value)
+    if ValueType == "string" then
+        return Value
+    end
+    if ValueType == "number" then
+        local AsString = tostring(Value)
+        if AsString:find("[eE]") then
+            return string.format("%.0f", Value)
+        end
+        return AsString
+    end
+    return tostring(Value)
+end
+
+local function NormalizeMap(Map)
+    if not Map then return Map end
+    Map.id = SafeNumberToString(Map.id)
+    return Map
+end
+
+local function NormalizeTime(Time)
+    if not Time then return Time end
+    Time.id = SafeNumberToString(Time.id)
+    if Time.map then
+        Time.map = NormalizeMap(Time.map)
+    end
+    return Time
+end
+
+local function FormatHelper(Value, Width)
+    Value = tostring(Value)
+    while #Value < Width do
+        Value = "0" .. Value
+    end
+    return Value
+end
+
+local function FormatTime(Milliseconds)
+    if Milliseconds > 86400000 then
+        return ">1 day"
+    end
+
+    local Ms = FormatHelper(Milliseconds % 1000, 3)
+    local Sec = FormatHelper(math.floor(Milliseconds / 1000) % 60, 2)
+    local Min = FormatHelper(math.floor(Milliseconds / (1000 * 60)) % 60, 2)
+    local Hour = FormatHelper(math.floor(Milliseconds / (1000 * 60 * 60)) % 24, 2)
+
+    if Hour == "00" then
+        return Min .. ":" .. Sec .. "." .. Ms
+    end
+
+    return Hour .. ":" .. Min .. ":" .. Sec
+end
+
+local function FormatSkill(Skill)
+    return Round(Skill * 100, 3) .. "%"
+end
+
+local STRAFESNET_API_URL = "https://api.strafes.net/api/v1/"
+local STRAFESNET_MAP_API_URL = "https://maps.strafes.net/public-api/v1/"
+local FIVEMAN_API_URL = 'https://api.fiveman1.net/v1/'
+local ROBLOX_API_URL = 'https://users.roblox.com/v1/'
+local ROBLOX_BADGES_API = 'https://badges.roblox.com/v1/'
+local ROBLOX_PRESENCE_URL = 'https://presence.roblox.com/v1/'
+local ROBLOX_THUMBNAIL_URL = 'https://thumbnails.roblox.com/v1/'
+local ROBLOX_INVENTORY_API = 'https://inventory.roblox.com/v1/'
+local ROBLOX_GROUPS_ROLES_URL = 'https://groups.roblox.com/v2/users/%s/groups/roles'
 
 ROBLOX_THUMBNAIL_SIZES = {
     [48] = '48x48',
@@ -62,6 +121,9 @@ local STRAFESNET_API_ENDPOINTS = {
         WORLD_RECORD = {
             GET = "time/worldrecord"
         },
+        PLACEMENT = {
+            GET = "time/placement"
+        },
         GET = "time/%d"
     },
     USERS = {
@@ -74,7 +136,54 @@ local STRAFESNET_API_ENDPOINTS = {
 }
 
 local StrafesNET = {}
+
+local GAME_IDS = {
+    BHOP = 1,
+    SURF = 2,
+    -- FLY_TRIALS = 5
+}
+local GAME_IDS_STRING = {
+    "Bhop",
+    "Surf",
+    -- [5] = "Fly Trials"
+}
+
+local STYLES = {
+    AUTOHOP = 1,
+    SCROLL = 2,
+    SIDEWAYS = 3,
+    HSW = 4,
+    WONLY = 5,
+    AONLY = 6,
+    BACKWARDS = 7,
+    FASTE = 8,
+    -- LOW_GRAVITY = 14
+}
+local STYLES_STRING = {
+    "Autohop",
+    "Scroll",
+    "Sideways",
+    "Half-Sideways",
+    "W-Only",
+    "A-Only",
+    "Backwards",
+    "Faste",
+    -- [14] = "Low Gravity"
+}
+local BHOP_STYLES = {STYLES.AUTOHOP, STYLES.SCROLL, STYLES.SIDEWAYS, STYLES.HSW, STYLES.WONLY, STYLES.AONLY, STYLES.BACKWARDS, STYLES.FASTE}
+local SURF_STYLES = {STYLES.AUTOHOP, STYLES.SIDEWAYS, STYLES.HSW, STYLES.WONLY, STYLES.AONLY, STYLES.BACKWARDS, STYLES.FASTE}
+
+StrafesNET.SafeNumberToString = SafeNumberToString
+
+StrafesNET.FormatTime = FormatTime
+StrafesNET.FormatSkill = FormatSkill
+
 StrafesNET.GameIds = GAME_IDS
+StrafesNET.GameIdsString = GAME_IDS_STRING
+StrafesNET.Styles = STYLES
+StrafesNET.StylesString = STYLES_STRING
+StrafesNET.BhopStyles = BHOP_STYLES
+StrafesNET.SurfStyles = SURF_STYLES
 
 function StrafesNET.CalculatePoints(Rank, Count)
     local ExpMagic2 = math.exp(RankConstants.Magic2)
@@ -101,15 +210,15 @@ function StrafesNET.CalculateSkill(Rank, Count)
 end
 
 function StrafesNET.ListMaps(GameId, PageSize, PageNumber)
-    local RequestUrl = STRAFESNET_API_URL .. STRAFESNET_API_ENDPOINTS.MAPS.LIST
+    local RequestUrl = STRAFESNET_MAP_API_URL .. STRAFESNET_API_ENDPOINTS.MAPS.LIST
     local Params = { game_id = GameId, page_size = PageSize or 10, page_number = PageNumber or 1 }
-    return Request("GET", RequestUrl, Params, RequestHeaders)
+    return Request("GET", RequestUrl, Params, RequestHeaders, {CacheTTL = 60 * 60 * 12})
 end
 
 function StrafesNET.GetMap(MapId)
-    local RequestUrl = STRAFESNET_API_URL .. STRAFESNET_API_ENDPOINTS.MAPS.GET:format(MapId)
+    local RequestUrl = STRAFESNET_MAP_API_URL .. STRAFESNET_API_ENDPOINTS.MAPS.GET:format(MapId)
     local Params = { id = MapId }
-    return Request("GET", RequestUrl, Params, RequestHeaders)
+    return Request("GET", RequestUrl, Params, RequestHeaders, {CacheTTL = 60 * 60 * 12})
 end
 
 function StrafesNET.ListRanks(GameId, ModeId, StyleId, SortBy, PageSize, PageNumber)
@@ -122,7 +231,7 @@ function StrafesNET.ListRanks(GameId, ModeId, StyleId, SortBy, PageSize, PageNum
         page_size = PageSize or 10,
         page_number = PageNumber or 1
     }
-    return Request("GET", RequestUrl, Params, RequestHeaders)
+    return Request("GET", RequestUrl, Params, RequestHeaders, {CacheTTL = 60 * 10})
 end
 
 function StrafesNET.ListTimes(MapId, GameId, ModeId, StyleId, UserId, SortBy, PageSize, PageNumber)
@@ -137,7 +246,7 @@ function StrafesNET.ListTimes(MapId, GameId, ModeId, StyleId, UserId, SortBy, Pa
         page_size = PageSize or 10,
         page_number = PageNumber or 1
     }
-    return Request("GET", RequestUrl, Params, RequestHeaders)
+    return Request("GET", RequestUrl, Params, RequestHeaders, {CacheTTL = 60 * 60 * 24 * 7})
 end
 
 function StrafesNET.GetWorldRecords(UserId, MapId, GameId, ModeId, StyleId, PageSize, PageNumber)
@@ -151,12 +260,53 @@ function StrafesNET.GetWorldRecords(UserId, MapId, GameId, ModeId, StyleId, Page
         page_size = PageSize or 10,
         page_number = PageNumber or 0
     }
-    return Request("GET", RequestUrl, Params, RequestHeaders)
+    return Request("GET", RequestUrl, Params, RequestHeaders, {CacheTTL = 60 * 10})
+end
+
+function StrafesNET.GetTimePlacement(TimeIds)
+    local RequestUrl = STRAFESNET_API_URL .. STRAFESNET_API_ENDPOINTS.TIMES.PLACEMENT.GET
+    local Params = {
+        ids = TimeIds
+    }
+    return Request("GET", RequestUrl, Params, RequestHeaders, {CacheTTL = 60 * 60})
+end
+
+function StrafesNET.GetAllTimePlacements(TimeIds)
+    local Placements = {}
+    if not TimeIds or #TimeIds == 0 then
+        return Placements
+    end
+
+    local BatchSize = 25
+    local Index = 1
+
+    while Index <= #TimeIds do
+        local Batch = {}
+        for BatchIndex = Index, math.min(Index + BatchSize - 1, #TimeIds) do
+            Batch[#Batch + 1] = TimeIds[BatchIndex]
+        end
+
+        local Headers, Response = StrafesNET.GetTimePlacement(Batch)
+        if Headers.code and Headers.code >= 400 then
+            return error("HTTP error while getting time placements")
+        end
+
+        if Response and Response.data then
+            for _, Item in next, Response.data do
+                    local TimeId = SafeNumberToString(Item.id)
+                    Placements[TimeId] = Item.placement
+            end
+        end
+
+        Index = Index + BatchSize
+    end
+
+    return Placements
 end
 
 function StrafesNET.GetTime(TimeId)
     local RequestUrl = STRAFESNET_API_URL .. STRAFESNET_API_ENDPOINTS.TIMES.GET:format(TimeId)
-    return Request("GET", RequestUrl, nil, RequestHeaders)
+    return Request("GET", RequestUrl, nil, RequestHeaders, {CacheTTL = 60 * 60 * 6})
 end
 
 function StrafesNET.ListUsers(StateId, PageSize, PageNumber)
@@ -166,12 +316,12 @@ function StrafesNET.ListUsers(StateId, PageSize, PageNumber)
         page_size = PageSize or 10,
         page_number = PageNumber or 1,
     }
-    return Request("GET", RequestUrl, Params, RequestHeaders)
+    return Request("GET", RequestUrl, Params, RequestHeaders, {CacheTTL = 60 * 10})
 end
 
 function StrafesNET.GetUser(UserId)
     local RequestUrl = STRAFESNET_API_URL .. STRAFESNET_API_ENDPOINTS.USERS.GET:format(UserId)
-    return Request("GET", RequestUrl, nil, RequestHeaders)
+    return Request("GET", RequestUrl, nil, RequestHeaders, {CacheTTL = 60 * 10})
 end
 
 function StrafesNET.GetUserRank(UserId, GameId, ModeId, StyleId)
@@ -181,7 +331,7 @@ function StrafesNET.GetUserRank(UserId, GameId, ModeId, StyleId)
         mode_id = ModeId,
         style_id = StyleId,
     }
-    return Request("GET", RequestUrl, Params, RequestHeaders)
+    return Request("GET", RequestUrl, Params, RequestHeaders, {CacheTTL = 60 * 10})
 end
 
 -- util stuff or something
@@ -193,6 +343,14 @@ function StrafesNET.GetMapCompletionCount(MapId, GameId, ModeId, StyleId)
     return Response.pagination.total_items
 end
 
+function StrafesNET.GetMapsCompletionCounts(MapIds, GameId, ModeId, StyleId)
+    local CompletionCounts = {}
+    for _, MapId in next, MapIds do
+        CompletionCounts[MapId] = StrafesNET.GetMapCompletionCount(MapId, GameId, ModeId, StyleId)
+    end
+    return CompletionCounts
+end
+
 function StrafesNET.GetAllUserTimes(UserId, GameId, ModeId, StyleId)
     local Times = {}
     local CurrentPage = 1
@@ -201,6 +359,7 @@ function StrafesNET.GetAllUserTimes(UserId, GameId, ModeId, StyleId)
         return error("HTTP error while getting times for something")
     end
     for TimeIndex, Time in next, Response.data do
+        NormalizeTime(Time)
         Times[Time.id] = Time
     end
     
@@ -213,6 +372,7 @@ function StrafesNET.GetAllUserTimes(UserId, GameId, ModeId, StyleId)
             return error("HTTP error while getting times for something")
         end
         for _, Time in next, _Response.data do
+            NormalizeTime(Time)
             Times[Time.id] = Time
         end
     end
@@ -221,14 +381,58 @@ function StrafesNET.GetAllUserTimes(UserId, GameId, ModeId, StyleId)
 end
 
 function StrafesNET.GetAllMaps()
-    local Maps = {}
-    -- TODO
-    return Maps
+    if StrafesNET.Maps ~= nil then
+        return StrafesNET.Maps
+    end
+
+    StrafesNET.Maps = {
+        [GAME_IDS.BHOP] = {},
+        [GAME_IDS.SURF] = {}
+    }
+
+    local PageSize = 100
+    local Page = 1
+
+    local LastPage = false
+    while not LastPage do
+        local _, Body = StrafesNET.ListMaps(GAME_IDS.BHOP, PageSize, Page)
+        local MapsData = Body.data
+        for MapIndex = 1, #MapsData do
+            local Map = MapsData[MapIndex]
+                local Map = NormalizeMap(MapsData[MapIndex])
+                local MapId = Map.id
+            StrafesNET.Maps[GAME_IDS.BHOP][MapId] = Map
+        end
+        Page = Page + 1
+        if #MapsData < PageSize then
+            LastPage = true
+        end
+    end
+
+    LastPage = false
+    Page = 1
+
+    while not LastPage do
+        local _, Body = StrafesNET.ListMaps(GAME_IDS.SURF, PageSize, Page)
+        local MapsData = Body.data
+        for MapIndex = 1, #MapsData do
+            local Map = MapsData[MapIndex]
+                local Map = NormalizeMap(MapsData[MapIndex])
+                local MapId = Map.id
+            StrafesNET.Maps[GAME_IDS.SURF][MapId] = Map
+        end
+        Page = Page + 1
+        if #MapsData < PageSize then
+            LastPage = true
+        end
+    end
+
+    return StrafesNET.Maps
 end
 
 function StrafesNET.GetRobloxInfoFromUserId(USER_ID)
     if not USER_ID then return 'empty id' end
-    return Request("GET", ROBLOX_API_URL .. "users/" .. USER_ID)
+    return Request("GET", ROBLOX_API_URL .. "users/" .. USER_ID, {CacheTTL = 60 * 60 * 12})
 end
 
 function StrafesNET.GetRobloxInfoFromUsername(USERNAME)
@@ -236,7 +440,7 @@ function StrafesNET.GetRobloxInfoFromUsername(USERNAME)
     if #USERNAME > 32 then return 'Username too long' end
 
     local headers, body = Request("POST", ROBLOX_API_URL .. "usernames/users", nil,
-        { ["Content-Type"] = "application/json" }, { usernames = { USERNAME } })
+        { ["Content-Type"] = "application/json" }, { usernames = { USERNAME } }, {CacheTTL = 60 * 60 * 12})
     if not body or not body.data or not body.data[1] then
         return 'Username \'' .. USERNAME .. '\' not found.'
     end
@@ -247,10 +451,10 @@ end
 function StrafesNET.GetRobloxInfoFromDiscordId(DISCORD_ID)
     if not DISCORD_ID then return 'empty id' end
     -- table.foreach(DISCORD_ID, print)
-    local headers, body = Request("GET", FIVEMAN_API_URL .. "users/" .. DISCORD_ID)
+    local headers, body = Request("GET", FIVEMAN_API_URL .. "users/" .. DISCORD_ID, {CacheTTL = 60 * 60 * 24 * 7})
     if headers.status == "error" then return headers.messages end
 
-    return Request("GET", ROBLOX_API_URL .. "users/" .. body.result.robloxId)
+    return StrafesNET.GetRobloxInfoFromUserId(body.result.robloxId)
 end
 
 function StrafesNET.GetUserOnlineStatus(USER_ID)
@@ -269,23 +473,21 @@ end
 function StrafesNET.GetUserUsernameHistory(USER_ID)
     if not USER_ID then return 'empty id' end
     return Request("GET", ROBLOX_API_URL .. "users/" .. USER_ID .. "/username-history",
-        { limit = 50, sortOrder = 'Desc' })
+        { limit = 50, sortOrder = 'Desc' }, {CacheTTL = 60 * 60 * 24})
 end
 
 function StrafesNET.GetBadgesAwardedDates(USER_ID, BADGE_LIST)
     if not USER_ID then return 'empty id' end
     return Request("GET", ROBLOX_BADGES_API .. "users/" .. USER_ID .. "/badges/awarded-dates",
-        { badgeIds = table.concat(BADGE_LIST, ",") })
+        { badgeIds = table.concat(BADGE_LIST, ",") }, {CacheTTL = 60 * 60 * 24 * 365})
 end
 
 function StrafesNET.GetVerificationItemID(USER_ID)
     if not USER_ID then return 'empty id' end
 
-    local headers1, body1 = Request("GET", ROBLOX_INVENTORY_API .. "users/" .. USER_ID .. "/items/Asset/102611803")
-    if body1.errors then return body1 end
+    local headers1, body1 = Request("GET", ROBLOX_INVENTORY_API .. "users/" .. USER_ID .. "/items/Asset/102611803", {CacheTTL = 60 * 60 * 24 * 7, MaxRetries = 0})
 
-    local headers2, body2 = Request("GET", ROBLOX_INVENTORY_API .. "users/" .. USER_ID .. "/items/Asset/1567446")
-    if body2.errors then return body2 end
+    local headers2, body2 = Request("GET", ROBLOX_INVENTORY_API .. "users/" .. USER_ID .. "/items/Asset/1567446", {CacheTTL = 60 * 60 * 24 * 7, MaxRetries = 0})
 
     local data = {}
     if body2.data and body2.data[1] then data[#data + 1] = body2.data[1] end
@@ -301,7 +503,7 @@ function StrafesNET.GetUserThumbnail(USER_ID, TYPE, SIZE)
     local _SIZE = ROBLOX_THUMBNAIL_SIZES[SIZE] or "180x180"
 
     return Request("GET", ROBLOX_THUMBNAIL_URL .. "users/" .. _TYPE,
-        { userIds = USER_ID, size = _SIZE, format = "Png", isCircular = false })
+        { userIds = USER_ID, size = _SIZE, format = "Png", isCircular = false }, {CacheTTL = 60 * 60 * 4})
 end
 
 return StrafesNET
--- src/SlashCommands/Points&SkillCalculation.lua
+++ src/SlashCommands/Points&SkillCalculation.lua
@@ -5,17 +5,40 @@ local Date = Discordia.Date
 Discordia.extensions()
 local StrafesNET = require('../Modules/StrafesNET.lua')
 
+local SafeNumberToString = StrafesNET.SafeNumberToString
+
+local StrafesNETMaps = StrafesNET.GetAllMaps()
+
 local CalculateCommand = SlashCommandTools.slashCommand('calculate', 'Calculate rank and skill points')
 
 local UsernameOption = SlashCommandTools.string('username', 'Username to look up')
 local UserIdOption = SlashCommandTools.integer('user_id', 'User ID to look up')
 local MemberOption = SlashCommandTools.user('member', 'User to look up')
 
-local GameIdOption = SlashCommandTools.integer
+local GameOption = SlashCommandTools.string("game", "Which game to use times from")
+for Game, GameId in next, StrafesNET.GameIds do
+    GameOption = GameOption:addChoice(SlashCommandTools.choice(StrafesNET.GameIdsString[GameId], Game))
+end
+GameOption = GameOption:setRequired(true)
+
+local StyleOption = SlashCommandTools.string("style", "Which style to use times from")
+for Style, StyleId in next, StrafesNET.Styles do
+    StyleOption = StyleOption:addChoice(SlashCommandTools.choice(StrafesNET.StylesString[StyleId], Style))
+end
+StyleOption = StyleOption:setRequired(true)
+
+CalculateCommand:addOption(GameOption)
+CalculateCommand:addOption(StyleOption)
 
 CalculateCommand:addOption(UsernameOption)
 CalculateCommand:addOption(UserIdOption)
 CalculateCommand:addOption(MemberOption)
+
+local function Pad(String, Padding)
+    Padding = Padding or 20
+    String = tostring(String)
+    return String..string.rep(" ", Padding-#String)
+end
 
 local function Callback(Interaction, Command, Args)
     local UserInfo
@@ -46,8 +69,99 @@ local function Callback(Interaction, Command, Args)
     if UserInfo == nil then
         error("SOMETHING WENT REALLY WRONG")
     end
+    
+    Interaction:replyDeferred()
+    
+    local GameId, StyleId = StrafesNET.GameIds[Args.game], StrafesNET.Styles[Args.style]
 
-    -- Add args for game/style etc and grab all times and grab all placements
+    local GameMaps = StrafesNETMaps[GameId]
+
+    local NTimes = 0
+
+    local Times = StrafesNET.GetAllUserTimes(UserInfo.id, GameId, 0, StyleId)
+    local TimeIds = {}
+    for _, Time in next, Times do
+        local MapId = SafeNumberToString(Time.map.id)
+        local Map = GameMaps[MapId]
+        local MapDate = Map and Map.date and Date.fromISO(Map.date):toSeconds() or nil
+        if MapDate and MapDate < os.time() then
+            NTimes = NTimes + 1
+            table.insert(TimeIds, SafeNumberToString(Time.id))
+        end
+    end
+    local TimePlacements = StrafesNET.GetAllTimePlacements(TimeIds)
+
+    local MapIds = {}
+    for _, Time in next, Times do
+        local MapId = SafeNumberToString(Time.map.id)
+        local Map = GameMaps[MapId]
+        local MapDate = Map and Map.date and Date.fromISO(Map.date):toSeconds() or nil
+        if MapDate and MapDate < os.time() then
+            table.insert(MapIds, MapId)
+        end
+    end
+    local MapCompletions = StrafesNET.GetMapsCompletionCounts(MapIds, GameId, 0, StyleId)
+
+    local TotalPoints = 0
+    local TimesConcise = {}
+
+    for _, Time in next, Times do
+        local Placement = TimePlacements[SafeNumberToString(Time.id)]
+        local MapId = SafeNumberToString(Time.map.id)
+        local MapCompletion = MapCompletions[MapId]
+        local Map = GameMaps[MapId]
+        local MapDate = Date.fromISO(Map.date):toSeconds()
+        if MapDate and MapDate < os.time() and Placement and MapCompletion then
+            local RankPoints = StrafesNET.CalculatePoints(Placement, MapCompletion)
+            local Skill = StrafesNET.CalculateSkill(Placement, MapCompletion)
+
+            TotalPoints = TotalPoints + RankPoints
+
+            table.insert(TimesConcise, {
+                Time = Time,
+                Placement = Placement,
+                MapCompletion = MapCompletion,
+                RankPoints = RankPoints,
+                Skill = Skill
+            })
+        end
+    end
+
+    table.sort(TimesConcise, function(Time1, Time2)
+        return Time1.RankPoints < Time2.RankPoints
+    end)
+
+    local FinalText = "Rank calculation for " .. UserInfo.displayName .. " (@" .. UserInfo.name .. ") / " .. UserInfo.id .. ": ".. StrafesNET.GameIdsString[GameId]..", "..StrafesNET.StylesString[StyleId] .."\n" ..
+                        "Points: " .. TotalPoints .. "\n" ..
+                        "Times: " .. NTimes .. "\n\n" ..
+                        Pad("Map", 50) .. " | " .. Pad("Points") .. " | " .. Pad("Skill", 7) .. " | " .. Pad("Placement", 14) .. " | Time\n"
+
+    for _, TimeData in next, TimesConcise do
+        local Placement = TimeData.Placement
+        local MapCompletion = TimeData.MapCompletion
+        local PlacementString = Placement .. "/" .. MapCompletion
+
+        local RankPoints = TimeData.RankPoints
+        local Skill = StrafesNET.FormatSkill(TimeData.Skill)
+
+        local TimeString = StrafesNET.FormatTime(TimeData.Time.time)
+        
+        local Map = TimeData.Time.map
+        local MapString = Map.display_name .. " (" .. Map.id .. ")"
+
+        FinalText = FinalText .. Pad(MapString, 50) .. " | " .. Pad(RankPoints) .. " | " .. Pad(Skill, 7) .. " | " .. Pad(PlacementString, 14) .. " | " .. TimeString .. "\n"
+    end
+
+    local FileName = "./rank-" .. StrafesNET.GameIdsString[GameId]:lower() .. "-" .. StrafesNET.StylesString[StyleId]:lower() .. "-" .. UserInfo.name .. "-" .. UserInfo.id .. ".txt"
+    local FileHandle = io.open(FileName, "w+")
+    FileHandle:write(FinalText)
+    FileHandle:close()
+
+    Interaction:reply({
+        file = FileName
+    })
+    
+    os.remove(FileName)
 end
 
 return {
