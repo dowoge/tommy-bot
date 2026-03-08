1bff1398d69894ddf7f70f536b99e8f0ad81a0ae
tommy <t@tommyy.dev>
2026-03-08 18:17:06 -0400

    slight oversights

--- src/SlashCommands/Update.lua
+++ src/SlashCommands/Update.lua
@@ -6,24 +6,22 @@ local OPERATING_SYSTEM = package.loaded.jit.os
 local START_FILE_TRANSLATIONS = {
 	Windows = {
 		FileName = "start.bat",
-
+		RestartCommand = 'start "" cmd /c "(timeout /t 3 /nobreak > NUL && call .\\start.bat)"'
 	},
 	Linux = {
-		FileName = "start.sh"
-	}
-}
-local WAIT_TRANSLATIONS = {
-	Windows = {
-		FileName = "timeout /t",
-	},
-	Linux = {
-		FileName = "sleep",
+		FileName = "start.sh",
+		RestartCommand = 'sh -c "(sleep 3; nohup sh ./start.sh >/dev/null 2>&1 &) "'
 	}
 }
 
 local function Callback(Interaction, Command, Args)
 	if Interaction.user.id ~= "697004725123416095" then
 		return Interaction:reply('You do not have permission to use this command.')
+	end
+
+	local PlatformConfig = START_FILE_TRANSLATIONS[OPERATING_SYSTEM]
+	if not PlatformConfig then
+		return Interaction:reply('Unsupported operating system.', true)
 	end
 
 	-- pull
@@ -36,10 +34,13 @@ local function Callback(Interaction, Command, Args)
 		return
 	end
 
-	-- start the bot after 3 seconds
-	local StartFileName = START_FILE_TRANSLATIONS[OPERATING_SYSTEM].FileName
-	local WaitString = WAIT_TRANSLATIONS[OPERATING_SYSTEM].FileName
-	io.popen(string.format('%s 3 && %s', WaitString, StartFileName)):close()
+	-- start the bot after 3 seconds in a detached process so it survives this process exiting
+	local RestartHandle = io.popen(PlatformConfig.RestartCommand)
+	if RestartHandle then
+		RestartHandle:close()
+	else
+		return Interaction:followUp('Failed to schedule restart.', true)
+	end
 
 	os.exit()
 end
